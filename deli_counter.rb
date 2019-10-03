58e0215f6bfd60054c218198d1c8ccf17bb866c0
Ally Morson <ally.morson@gmail.com>
2019-10-03 23:14:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,25 @@
-# Write your code here.
-katz_deli = []
-if []
-  puts "The line is currently empty"
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:" #must have assignment variable current_line so there is something to call on
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"  
+    end
+    puts current_line
+  end
 end
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}."
+    deli.shift
+  end
 end
