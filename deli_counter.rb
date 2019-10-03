33d41abe0944ad6ccc2ad332bf50040e50527739
Ally Morson <ally.morson@gmail.com>
2019-10-03 23:25:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(deli)
     deli.each.with_index(1) do |person, i|
       current_line << " #{i}. #{person}"  
     end
-    puts current_line
+    puts current_line #must call current_line or the original array will return only without the << #{i} and #{person}
   end
 end
 
